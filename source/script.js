858d1d8258fb4ed4a28f87ae6ba42c808e5a6907
Max S. Haberman <max@surveycto.com>
2020-04-29 15:32:40 -0400

    Updated JS according to StandardJs

--- source/script.js
+++ source/script.js
@@ -1,46 +1,43 @@
-var isAndroid = (document.body.className.indexOf("android-collect") >= 0);
-var phoneNumber = getPluginParameter('phone_number');
-var buttonLabel = getPluginParameter('button_label');
-var previewNumberContainer = document.getElementById('phone-preview-number-container');
-var btnCallPhone = document.getElementById('btn-call-phone');
-var statusContainer = document.getElementById('status-container');
-var currentAnswer = fieldProperties.CURRENT_ANSWER || "";
+var isAndroid = (document.body.className.indexOf('android-collect') >= 0)
+var phoneNumber = getPluginParameter('phone_number')
+var buttonLabel = getPluginParameter('button_label')
+var previewNumberContainer = document.getElementById('phone-preview-number-container')
+var btnCallPhone = document.getElementById('btn-call-phone')
+var statusContainer = document.getElementById('status-container')
+var currentAnswer = fieldProperties.CURRENT_ANSWER || ''
 
 // set the preview values to show what has been passed from the parameters
-previewNumberContainer.innerHTML = phoneNumber;
-btnCallPhone.innerText = buttonLabel || "Click here to call"; 
+previewNumberContainer.innerHTML = phoneNumber
+btnCallPhone.innerText = buttonLabel || 'Click here to call'
 if (!isAndroid) {
-    //btnCallPhone.disabled = true;
+  // btnCallPhone.disabled = true
 }
 
-// define what the "Call phone" button does
+// define what the 'Call phone' button does
 if (isAndroid) {
-    btnCallPhone.onclick = function() {
-        launchCallUsingAndroidIntent();
-    }
+  btnCallPhone.onclick = function () {
+    launchCallUsingAndroidIntent()
+  }
 } else {
-    btnCallPhone.setAttribute("href", "tel:" + phoneNumber);
+  btnCallPhone.setAttribute('href', 'tel:' + phoneNumber)
 }
 
-
-function launchCallUsingAndroidIntent() {
-    // set the parameters for the intent
-    var params = {
-        uri_data: 'tel:' + phoneNumber
-    };
-    // Launches the "android.intent.action.DIAL" intent using the parameters above.
-    launchIntent('android.intent.action.DIAL', params, function(error, result) {
-        // Something went wrong while launching the intent.
-        if (error) {
-            currentAnswer += "[" + new Date().toLocaleString() + "] Failure calling the following phone number: " + phoneNumber + ".\n";
-            setAnswer(currentAnswer);
-            statusContainer.innerHTML = error;
-            return;
-        }
-        else {
-            currentAnswer += "[" + new Date().toLocaleString() + "] The following phone number was called: " + phoneNumber + ".\n";
-            setAnswer(currentAnswer);
-            statusContainer.innerHTML = "Success!";
-        }
-    });
+function launchCallUsingAndroidIntent () {
+  // set the parameters for the intent
+  var params = {
+    uri_data: 'tel:' + phoneNumber
+  }
+  // Launches the 'android.intent.action.DIAL' intent using the parameters above.
+  launchIntent('android.intent.action.DIAL', params, function (error, result) {
+    // Something went wrong while launching the intent.
+    if (error) {
+      currentAnswer += '[' + new Date().toLocaleString() + '] Failure calling the following phone number: ' + phoneNumber + '.\n'
+      setAnswer(currentAnswer)
+      statusContainer.innerHTML = error
+    } else {
+      currentAnswer += '[' + new Date().toLocaleString() + '] The following phone number was called: ' + phoneNumber + '.\n'
+      setAnswer(currentAnswer)
+      statusContainer.innerHTML = 'Success!'
+    }
+  })
 }
